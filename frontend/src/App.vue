b77b1b5b29689d0e9e21a35b244e9400a4be3afa
Jiyoon-park <soundpjy1205@gmail.com>
2020-07-20 10:48:39 +0900

    S03P12A404 fe Add init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/login">Login</router-link>|
+      <router-link to="/signup">Signup</router-link>|
+      <router-link to="/findpw">findpw</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
---
+++ frontend/src/views/user/Findpw.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="findpw">
+    <h1>비밀번호 찾기</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Findpw",
+  components: {}
+};
+</script>
