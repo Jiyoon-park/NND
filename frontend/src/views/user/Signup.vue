6c804b9db0520e7ad1279d4a1b378f2c397c3064
okk0 <okk0@naver.com>
2020-07-20 11:00:22 +0900

    S03P12A404 fe Edit Signup

--- frontend/src/views/user/Signup.vue
+++ frontend/src/views/user/Signup.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="signup">
+    <!-- <img
+        class="logo"
+        src="../assets/images/logo_without_title.png"
+        alt="logo"
+        width="30%"
+      /> -->
     <h1>회원가입</h1>
+    <div class="join-form">
+        <div class="input">
+          <label for="email">이메일 : </label>
+          <input v-model="email" id="email" placeholder="이메일을 입력하세요." type="text" />
+        </div>
+        <div class="input">
+          <label for="password">비밀번호 : </label>
+          <input v-model="email" id="email" placeholder="이메일을 입력하세요." type="text" />
+        </div>
+        <div class="input">
+          <label for="email">이메일 : </label>
+          <input v-model="email" id="email" placeholder="이메일을 입력하세요." type="text" />
+        </div>
+      </div>
   </div>
+  
 </template>
 
 <script>
