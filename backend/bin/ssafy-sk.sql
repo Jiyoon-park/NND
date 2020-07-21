e1708b20f08917a3e5d756688de2339d139f0c73
giseok oh <ohks9208@naver.com>
2020-07-21 17:23:11 +0900

    please please

--- backend/bin/ssafy-sk.sql
+++ backend/bin/ssafy-sk.sql
@@ -1,4 +1,4 @@
-# Dump of table user
+﻿# Dump of table user
 # ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `user`;
@@ -9,6 +9,17 @@ CREATE TABLE `user` (
   `password` varchar(128) DEFAULT NULL,
   `create_date` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`uid`),
+  UNIQUE KEY `user_idx_unique_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+// 따로 만든 `tb_member` 테이블
+CREATE TABLE `tb_member` (
+  `id` int(11) NOT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `user_idx_unique_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
