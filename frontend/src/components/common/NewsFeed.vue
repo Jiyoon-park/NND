fc894da1de27e93677f5f4726e401d8954fcd547
okk0 <okk0@naver.com>
2020-07-23 08:43:25 +0900

    s03p12a404 fe Edit NewsFeed

--- frontend/src/components/common/NewsFeed.vue
+++ frontend/src/components/common/NewsFeed.vue
@@ -1,37 +1,28 @@
 <template>
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-card-media
-          class="white--text"
-          height="200px"
-        >
-          <v-container fill-height fluid>
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="headline">Title</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-media>
-        <v-card-title>
-          <div>
-            <span class="grey--text">User Name</span><br>
-            <span>공모전 팀원 구합니다</span><br>
-            <span>같이 javascript 및 jQuery 공부할 팀원 구합니다</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="orange">상세정보 보기</v-btn>
-          <v-btn flat color="orange">즐겨찾기</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-title>
+            <div>
+              <v-icon class="mr-5">mdi-account-circle</v-icon>
+              <span class="grey--text">User Name</span>
+              <br />
+              <span>공모전 팀원 구합니다</span>
+              <br />
+              <span>같이 javascript 및 jQuery 공부할 팀원 구합니다</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">상세정보 보기</v-btn>
+            <v-btn flat color="orange">즐겨찾기</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
