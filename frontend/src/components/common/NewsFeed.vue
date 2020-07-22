8dea6152221bc59cc3636b44886fe6cd0297ad63
okk0 <okk0@naver.com>
2020-07-23 08:41:56 +0900

    s03p12a404 fe Edit NewsFeed

--- frontend/src/components/common/NewsFeed.vue
+++ frontend/src/components/common/NewsFeed.vue
@@ -5,6 +5,7 @@
         <v-card>
           <v-card-title>
             <div>
+              <v-icon class="mr-5">mdi-account-circle</v-icon>
               <span class="grey--text">User Name</span>
               <br />
               <span>공모전 팀원 구합니다</span>
@@ -14,7 +15,9 @@
           </v-card-title>
           <v-card-actions>
             <v-btn flat color="orange">상세정보 보기</v-btn>
-            <v-btn flat color="orange">즐겨찾기</v-btn>
+            <v-btn icon color="indigo">
+              <v-icon>mdi-star</v-icon>
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
