95aebcf3cacd5c99cf5765b7ad29de4e41878bed
Jiyoon-park <soundpjy1205@gmail.com>
2020-07-22 17:17:09 +0900

    S03P12A404-38 fe Add Tabs & SearchBar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Tabs />
+  </v-app>
 </template>
 
 <script>
+import Tabs from "../components/common/Tabs.vue";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Tabs
   }
-}
+};
 </script>
