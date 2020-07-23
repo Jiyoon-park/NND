c7e08db7c534e89b0a0a56e50493cf9b0916a346
okk0 <okk0@naver.com>
2020-07-23 17:05:08 +0900

    s03p12a404 fe Edit Tabs

--- frontend/src/components/common/Tabs.vue
+++ frontend/src/components/common/Tabs.vue
@@ -1,19 +1,17 @@
 <template>
   <v-app>
-    <v-card>
-      <v-tabs
-        class="rounded-0"
-        background-color="teal"
-        v-model="currentItem"
-        dark
-        fixed-tabs
-        slider-color="white"
-      >
-        <v-tab v-for="item in items" :key="item" :href="'#tab-' + item">{{
-          item
-        }}</v-tab>
-      </v-tabs>
-    </v-card>
+    <v-tabs
+      class="rounded-0 mt-13"
+      background-color="teal"
+      v-model="currentItem"
+      dark
+      fixed-tabs
+      slider-color="white"
+    >
+      <v-tab v-for="item in items" :key="item" :href="'#tab-' + item">{{
+        item
+      }}</v-tab>
+    </v-tabs>
 
     <v-tabs-items v-model="currentItem">
       <v-tab-item v-for="item in items" :key="item" :value="'tab-' + item">
