62ac1dd666c702eeb62f4dd2a3b8dacdc75668a5
Jihoon Ham <logbug1004@gmail.com>
2020-07-31 09:31:21 +0900

    S03P12A404-52 be Edit CREATE SQL

--- backend/src/main/resources/SQL/CREATE/likemember.sql
+++ backend/src/main/resources/SQL/CREATE/likemember.sql
@@ -3,6 +3,6 @@ CREATE TABLE likemember(
     mboard                     int    NOT NULL,    
     mno                        int    NOT NULL,
     createdate                datetime DEFAULT current_timestamp(),
-    FOREIGN KEY (mno) REFERENCES member (idx),
-    FOREIGN KEY (mboard) REFERENCES memberboard (boardno)
+    FOREIGN KEY (mno) REFERENCES member (idx) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (mboard) REFERENCES memberboard (boardno) ON UPDATE CASCADE ON DELETE CASCADE
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8;
