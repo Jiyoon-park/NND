02d56e41864c985303f329bf2d55e69805db44fe
ohgiseok <oks2238@gmail.com>
2020-08-04 10:13:36 +0900

    S03P13A404-6 be mod memberboard paging

--- backend/src/main/java/com/ssafy/nnd/controller/MemberBoardController.java
+++ backend/src/main/java/com/ssafy/nnd/controller/MemberBoardController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.nnd.dto.MemberBoard;
@@ -24,10 +26,8 @@ public class MemberBoardController {
     MemberBoardRepository memberBoardRepository;
 
     @GetMapping("/memberboard/list")
-    public List<MemberBoard> getAllMemberBoard(){
-    	
-    	
-    	return memberBoardRepository.findAllByOrderByBoardNoDesc();
+    public List<MemberBoard> getAllMemberBoard(@RequestParam("page") Long page,@RequestParam("size") Long size, final Pageable pageable){
+    	return memberBoardRepository.findAllByOrderByBoardNoDesc(pageable);
     }
     
     @GetMapping("/memberboard/{id}")
