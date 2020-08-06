279785a827172dc986888017a027d30a5e650fb3
Jiyoon-park <soundpjy1205@gmail.com>
2020-08-07 03:59:50 +0900

    s03p13a404 fe Update projecthistory & profile

---
+++ frontend/src/components/profile/ProjectHistoryListItem.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-list-item three-line>
+    <v-list-item-content>
+      <v-list-item-title class="headline mb-1">{{ historyinfo.projectName }}</v-list-item-title>
+      <v-list-item-subtitle>{{ historyinfo.summary }}</v-list-item-subtitle>
+      <div class="mt-3">링크 : {{ historyinfo.gitLink }}</div>
+      <div>
+        <v-btn @click="onDeleteBtn(`${historyinfo.historyNo}`)">삭제</v-btn>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="green" dark v-bind="attrs" v-on="on" fab small class="mr-1">수정</v-btn>
+          </template>
+          <v-card>
+            <v-card-title
+              class="headline font-weight-regular light-green lighten-1 white--text"
+            >project history</v-card-title>
+            <v-form>
+              <v-container>
+                <v-text-field
+                  v-model="historyinfo.projectName"
+                  filled
+                  label="프로젝트 이름"
+                  placeholder="내용을 입력해주세요."
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="historyinfo.summary"
+                  filled
+                  label="프로젝트 한줄 설명"
+                  placeholder="내용을 입력해주세요."
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="historyinfo.usedStack"
+                  filled
+                  label="기술 스택"
+                  placeholder="기타 기술스택 직접 입력하기"
+                  required
+                ></v-text-field>
+                <v-textarea
+                  v-model="historyinfo.content"
+                  filled
+                  label="상세 업무 및 성과"
+                  placeholder="내용을 입력해주세요."
+                  required
+                ></v-textarea>
+                <v-text-field
+                  v-model="historyinfo.gitLink"
+                  filled
+                  label="저장소 링크"
+                  placeholder="https://github.com/example"
+                  required
+                ></v-text-field>
+              </v-container>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialog=false">Close</v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="onEditBtn(`${historyinfo.historyNo}`)"
+                >Save</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-dialog>
+      </div>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: {
+    historyinfo: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  created() {
+    console.log(this.historyinfo.idx);
+  },
+  methods: {
+    onDeleteBtn(projecthistoryNo) {
+      axios
+        .delete(
+          `http://localhost:8080/projecthistory/delete/${projecthistoryNo}`
+        )
+        .then((res) => console.log(res));
+    },
+    onEditBtn(projecthistoryNo) {
+      axios
+        .post(
+          `http://localhost:8080/projecthistory/update/${projecthistoryNo}`,
+          {
+            idx: this.historyinfo.idx,
+            projectName: this.historyinfo.projectName,
+            summary: this.historyinfo.summary,
+            content: this.historyinfo.content,
+            usedStack: this.historyinfo.usedStack,
+            gitLink: this.historyinfo.gitLink,
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.dialog = false;
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.dialog = false;
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
