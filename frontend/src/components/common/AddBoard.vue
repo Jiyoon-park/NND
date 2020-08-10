b9d67ea70653787cadacdd1f0be2803b250d05ab
이상현 <okk0@naver.com>
2020-08-10 02:10:38 +0000

    Update AddBoard.vue

--- frontend/src/components/common/AddBoard.vue
+++ frontend/src/components/common/AddBoard.vue
@@ -215,7 +215,6 @@ export default {
       axios
         .put(url,obj,
         {
-          {
             headers: {
               Authorization: "Bearer " + token.data, // the token is a variable which holds the token
             },
