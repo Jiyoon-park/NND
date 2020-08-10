9f7ae4ccda1e95f9dd53c1513ffead1c4885fdce
okk0 <okk0@naver.com>
2020-08-10 16:54:25 +0900

    s03p13a404 fe Edit Gongmo

--- frontend/src/components/gongmo/gongmo.vue
+++ frontend/src/components/gongmo/gongmo.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-row class="fill-height">
+  <v-row class="mt-16">
+    <NavBar />
     <v-col>
       <v-sheet height="64">
         <v-toolbar flat color="white">
@@ -24,7 +25,39 @@
 </template>
 
 <script>
+import NavBar from "../common/NavBar.vue";
+import axios from "axios";
+
 export default {
+  name: "Gongmo",
+  components: {
+    NavBar,
+  },
+  created() {
+    let token = window.$cookies.get("kakao"); //nnd가 key인 쿠키 가져옴
+    console.log(token);
+    if (token) {
+      // kakao 토큰이
+      axios
+        .get("http://localhost:8080/userinfo", {
+          headers: {
+            Authorization: "Bearer " + token.data, // the token is a variable which holds the token
+          },
+        })
+        .then(
+          (response) => {
+            console.log(response);
+            window.$cookies.remove("kakao"); //kakao 쿠키 없애고 새로생성
+            window.$cookies.set("nnd", response.data, "2d"); //로그인시 쿠키 저장
+            //console.log(response.data.object.idx)
+            location.reload();
+          },
+          () => {
+            console.log("failed");
+          }
+        );
+    }
+  },
   data: () => ({
     focus: "",
     type: "month",
