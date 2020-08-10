bf3fdd5123dfce5a292b84618a1dce505ae8c162
okk0 <okk0@naver.com>
2020-08-10 11:06:28 +0900

    s03p13a404 fe Edit Gongmo

--- frontend/src/components/gongmo/gongmo.vue
+++ frontend/src/components/gongmo/gongmo.vue
@@ -1,5 +1,6 @@
 <template>
   <v-row class="fill-height">
+    <NavBar />
     <v-col>
       <v-sheet height="64">
         <v-toolbar flat color="white">
@@ -83,7 +84,12 @@
 </template>
 
 <script>
+import NavBar from "../common/NavBar.vue";
+
 export default {
+  components: {
+    NavBar,
+  },
   data: () => ({
     focus: "",
     type: "month",
@@ -153,35 +159,35 @@ export default {
 
       nativeEvent.stopPropagation();
     },
-    updateRange({ start, end }) {
-      const events = [];
+    // updateRange({ start, end }) {
+    //   const events = [];
 
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
+    //   const min = new Date(`${start.date}T00:00:00`);
+    //   const max = new Date(`${end.date}T23:59:59`);
+    //   const days = (max.getTime() - min.getTime()) / 86400000;
+    //   const eventCount = this.rnd(days, days + 20);
 
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
+    //   for (let i = 0; i < eventCount; i++) {
+    //     const allDay = this.rnd(0, 3) === 0;
+    //     const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+    //     const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+    //     const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+    //     const second = new Date(first.getTime() + secondTimestamp);
 
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-        });
-      }
+    //     events.push({
+    //       name: this.names[this.rnd(0, this.names.length - 1)],
+    //       start: first,
+    //       end: second,
+    //       color: this.colors[this.rnd(0, this.colors.length - 1)],
+    //       timed: !allDay,
+    //     });
+    //   }
 
-      this.events = events;
-    },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
-    },
+    //   this.events = events;
+    // },
+    // rnd(a, b) {
+    //   return Math.floor((b - a + 1) * Math.random()) + a;
+    // },
   },
 };
 </script>
