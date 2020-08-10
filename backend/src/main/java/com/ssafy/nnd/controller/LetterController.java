832a78db2d78ec0bea747f97938ab9464cae930f
ohgiseok <oks2238@gmail.com>
2020-08-10 22:18:11 +0900

    - mod letter controller

--- backend/src/main/java/com/ssafy/nnd/controller/LetterController.java
+++ backend/src/main/java/com/ssafy/nnd/controller/LetterController.java
@@ -52,8 +52,8 @@ public class LetterController {
 		for (int i = 0; i < teamList.size(); i++) {
 			Map<String, Object> map = new HashMap<String, Object>();
 			Object[] temp = (Object[]) teamList.get(i);
-			map.put("teamboardno", temp[0]);
-			map.put("teamname", temp[1]);
+			map.put("teamboardNo", temp[0]);
+			map.put("teamName", temp[1]);
 			datalist.add(map);
 		}
 		return datalist;
@@ -82,13 +82,14 @@ public class LetterController {
 			Object[] temp = (Object[]) letter.get(i);
 			map.put("name", temp[1]);
 			map.put("profile", temp[3]);
-			map.put("letterNo", temp[9]);
-			map.put("sendIdx", temp[10]);
-			map.put("receiveIdx", temp[11]);
-			map.put("content", temp[12]);
-			map.put("createDate", temp[13]);
-			map.put("read", temp[14]);
-			map.put("letterType", temp[15]);
+			map.put("letterNo", temp[8]);
+			map.put("sendIdx", temp[9]);
+			map.put("receiveIdx", temp[10]);
+			map.put("content", temp[11]);
+			map.put("createDate", temp[12]);
+			map.put("read", temp[13]);
+			map.put("letterType", temp[14]);
+			map.put("teamboardNo",temp[15] );
 			datalist.add(map);
 		}
 		return datalist;
@@ -105,13 +106,14 @@ public class LetterController {
 			Object[] temp = (Object[]) letter.get(i);
 			map.put("name", temp[1]);
 			map.put("profile", temp[3]);
-			map.put("letterNo", temp[9]);
-			map.put("sendIdx", temp[10]);
-			map.put("receiveIdx", temp[11]);
-			map.put("content", temp[12]);
-			map.put("createDate", temp[13]);
-			map.put("read", temp[14]);
-			map.put("letterType", temp[15]);
+			map.put("letterNo", temp[8]);
+			map.put("sendIdx", temp[9]);
+			map.put("receiveIdx", temp[10]);
+			map.put("content", temp[11]);
+			map.put("createDate", temp[12]);
+			map.put("read", temp[13]);
+			map.put("letterType", temp[14]);
+			map.put("teamboardNo",temp[15] );
 			datalist.add(map);
 		}
 		return datalist;
