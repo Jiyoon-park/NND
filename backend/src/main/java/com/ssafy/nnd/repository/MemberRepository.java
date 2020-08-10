547fcf0a8fbd27aa6ba2d7ddb03fe98458d04e2c
ohgiseok <oks2238@gmail.com>
2020-08-10 19:28:46 +0900

    - mod be member, letter

--- backend/src/main/java/com/ssafy/nnd/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/nnd/repository/MemberRepository.java
@@ -16,10 +16,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByEmail(String email);
     Optional<Member> findMemberByEmailAndCompany(String email, String company);
     Optional<Member> findMemberByIdx(long idx);
-  //SELECT COUNT(*) FROM 테이블
-  	@Query(value= " select count(*) from member where teamboardno = :tboardno",nativeQuery = true)
-  	Long countTeamMemberByTeamboardNo(@Param("tboardno") Long tboardno);
   
-  	List<Member> findByTeamboardno(Long teamboardno);
 }
 
