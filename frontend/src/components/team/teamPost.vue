81b11cac68610384c291c91bc7713db1afdba36c
ohgiseok <oks2238@gmail.com>
2020-08-11 17:59:06 +0900

    fe add teampost

--- frontend/src/components/team/teamPost.vue
+++ frontend/src/components/team/teamPost.vue
@@ -1,21 +1,54 @@
 <template>
   <v-card>
     <v-list class="transparent">
-      <v-list-item v-for="(post, index) in teamposts" :key="index + '_teamposts'">
-        <v-list-item-title>{{ post.wirter }}</v-list-item-title>
-
-        <v-list-item-icon>
-          <v-icon>{{ post.title }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-subtitle class="text-right">{{ post.notice }}</v-list-item-subtitle>
-      </v-list-item>
+      <v-list-item-group>
+        <teamPostItem
+          :item="teamposts"
+          v-for="(post,i) in teamposts"
+          :key="i"
+          :postinfo="item.teamposts[i]"
+        />
+      </v-list-item-group>
     </v-list>
   </v-card>
 </template>
 
 <script>
-export default {};
+import TeamPostItem from "./teamPostItem.vue";
+import axios from "axios";
+
+export default {
+  name: "TeamPost",
+  components: {
+    TeamPostItem,
+  },
+  data() {
+    return {
+      dialog: false,
+      teamposts: [],
+    };
+  },
+  props: {
+    userinfo: {},
+  },
+  created() {
+    let teamboardno = this.$route.params.teamboardno;
+    let token = window.$cookies.get("nnd");
+    axios
+      .get(`http://localhost:8080/teammenu/post/${teamboardno}`, {
+        headers: {
+          Authorization: "Bearer " + token.data, // the token is a variable which holds the token
+        },
+      })
+      .then((res) => {
+        console.log({ res });
+        this.teamposts = res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style>
