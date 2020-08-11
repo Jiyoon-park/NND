bd1a0189617dfa24d5e3a379748d5fab1c4cede1
ohgiseok <oks2238@gmail.com>
2020-08-11 09:12:11 +0900

    - be mod teamMenuController and teamregistRepository

--- backend/src/main/java/com/ssafy/nnd/repository/TeamRegistRepository.java
+++ backend/src/main/java/com/ssafy/nnd/repository/TeamRegistRepository.java
@@ -16,4 +16,9 @@ public interface TeamRegistRepository extends JpaRepository<TeamRegist,Long>{
 	@Query(value="select teamboard.teamboardno, teamname from teamboard JOIN teamregist ON teamboard.teamboardno=teamregist.teamboardno WHERE memberidx= :idx",nativeQuery=true)
 	List<Object> findTeamByIdx(@Param("idx") Long idx);
 	
+	@Query(value="select member.idx, member.name from member JOIN teamregist ON member.idx=teamregist.memberidx WHERE memberidx= :idx",nativeQuery=true)
+	List<Object> findMemberByTeamboardNo(@Param("idx") Long idx);
+	
+	@Query(value="",nativeQuery=true)
+	void deleteByTeamboardNoAndMemberIdx(@Param("teamboardno") Long teamboardno, @Param("memberidx") Long memberidx);
 }
