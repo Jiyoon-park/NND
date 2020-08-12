d0efd87899dbf2201055ffef9bb2bc08281cf17b
ohgiseok <oks2238@gmail.com>
2020-08-12 22:50:34 +0900

    fe add postupdate

--- frontend/src/components/team/teamPostDetail.vue
+++ frontend/src/components/team/teamPostDetail.vue
@@ -28,12 +28,43 @@
       <v-spacer></v-spacer>
       <v-btn color="green darken-1" text @click="changeDialog">닫기</v-btn>
       <!-- 나를 영입하는 편지인지/다른 사람이 우리 팀에 지원하는 편지인지 분기 해서 둘 중 하나만 보여줘야 함 -->
-      <v-btn
-        v-if="postinfo.memberIdx==userinfo.idx"
-        color="green darken-1"
-        text
-        @click="postUpdate()"
-      >수정하기</v-btn>
+      <v-dialog v-model="updateDialog" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-if="postinfo.memberIdx==userinfo.idx"
+            color="green darken-1"
+            v-bind="attrs"
+            v-on="on"
+            text
+          >수정하기</v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">게시판 글쓰기</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field label="제목*" required v-model="title"></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-textarea label="내용*" required v-model="content"></v-textarea>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select :items="['공지']" label="공지" v-model="notice"></v-select>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*꼭 필요한 항목 표시</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="updateDialog = false">닫기</v-btn>
+            <v-btn color="blue darken-1" text @click="postUpdate()">수정</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-btn
         v-if="postinfo.memberIdx==userinfo.idx"
         color="green darken-1"
@@ -49,6 +80,14 @@ import axios from "axios";
 
 export default {
   components: {},
+  data() {
+    return {
+      updateDialog: false,
+      title: this.postinfo.title,
+      content: this.postinfo.content,
+      notice: this.postinfo.notice,
+    };
+  },
   props: {
     postinfo: {
       type: Object,
@@ -62,7 +101,29 @@ export default {
   },
   // 수정 삭제 메소드
   methods: {
-    postUpdate() {},
+    postUpdate() {
+      this.updateDialog = false;
+      axios
+        .post(`http://localhost:8080/teammenu/post/update`, {
+          teamPostNo: this.postinfo.teamPostNo,
+          teamboardNo: this.postinfo.teamboardNo,
+          memberIdx: this.userinfo.idx,
+          writer: this.userinfo.name,
+          title: this.title,
+          content: this.content,
+          notice: this.notice,
+          createDate: "",
+        })
+        .then((res) => {
+          console.log(res.data);
+          this.$emit("changeDialog");
+          alert("성공");
+        })
+        .catch((err) => {
+          console.log(err.respose);
+          alert("수정실패");
+        });
+    },
     postDelete() {
       axios
         .delete(
