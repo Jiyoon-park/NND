8c0ab9e1a7e28c7c339d553e78d2befd444b61f6
kimsunghyun <tjdgus2319@gmail.com>
2020-08-18 11:13:44 +0900

    edit Home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -45,6 +45,12 @@ export default {
     }
     let ntoken = window.$cookies.get("nnd"); //nnd가 key인 쿠키 가져옴
     if (!ntoken) {
+      for (let index = 0; index < 500000000; index++) {
+        if (index % 100000000 === 0) console.log("loading");
+      }
+    }
+    ntoken = window.$cookies.get("nnd"); //nnd가 key인 쿠키 가져옴
+    if (!ntoken) {
       alert("로그인해주세요!");
       this.$store.commit("logout");
       window.$cookies.remove("nnd");
