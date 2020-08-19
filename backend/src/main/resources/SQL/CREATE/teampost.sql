4a7fd8f024d1410fa86731ea52478d0516f2933c
kimsunghyun <tjdgus2319@gmail.com>
2020-08-19 14:29:54 +0900

    "edit teamboard"

--- backend/src/main/resources/SQL/CREATE/teampost.sql
+++ backend/src/main/resources/SQL/CREATE/teampost.sql
@@ -5,8 +5,10 @@
   `writer` varchar(30) NOT NULL,
   `title` varchar(128) NOT NULL,
   `content` varchar(500) NOT NULL,
-  `createdate` varchar(128) NOT NULL,
+   `createdate` datetime DEFAULT current_timestamp(),,
   `notice` tinyint(1) not null,
+  `latitude` varchar(128) default NULL,
+  'longitude` varchar(128) default NULL,
   PRIMARY KEY (`teampostno`),
   FOREIGN KEY(`teamboardno`) REFERENCES teamboard(`teamboardno`) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(`memberidx`) REFERENCES member(`idx`) ON UPDATE CASCADE ON DELETE CASCADE
